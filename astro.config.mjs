0b2a562ab2772b9471c490407e14bc504d798687
Victor Martinez <vicmarc30@gmail.com>
2024-06-23 15:56:40 -0600

    chore: Update npm dependencies and Astro project files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind(), ],
-  output: 'server'
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel()
 });
