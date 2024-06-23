952fd6ace652cd54cc750f44c4b4b9c601e91ad6
Victor Martinez <vicmarc30@gmail.com>
2024-06-23 17:36:02 -0600

    refactor: Update font family in ButtonToServices and Header components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,15 +6,24 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			
+			fontFamily: {
+				fugazOne: ['Fugaz One', 'system-ui'],
+			},
 			colors: {
 				primary: "var(--color-primary)",
 				secondary: "var(--color-secondary)",
 				accent: "var(--color-accent)",
-				twitch: "var(--color-twitch)",
-				ice: "var(--color-twitch-ice)",
 			},
 		},
 	},
-	plugins: [animations],
+	plugins: [
+		animations,
+		({ addComponents }) => {
+			addComponents({
+			  ".cp-v": {
+				clipPath: "polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)",
+			  },
+			});
+		  },
+	],
 }
