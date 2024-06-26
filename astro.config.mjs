762507e2041e59a9039c80e62a490faaf885e146
Victor Martinez <vicmarc30@gmail.com>
2024-06-25 19:35:29 -0600

    refactor: Update tsconfig.json, package.json, and astro.config.mjs for Preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+import preact from "@astrojs/preact";
+
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
   output: 'server',
   adapter: vercel()
 });
